60ff3cc640ec69924745a2df57f6540a9f49225b
Oleg Nechiporenko <oo.nechiporenko@gmail.com>
2020-04-14 19:16:53 +0300

    Always call `this._super.included`

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ module.exports = {
 
   included(appOrAddon) {
     let app = appOrAddon.app || appOrAddon;
+    this._super.included.apply(this, arguments);
     if (!app.__emberBasicDropdownIncludedInvoked) {
       app.__emberBasicDropdownIncludedInvoked = true;
-      this._super.included.apply(this, arguments);
 
       let hasSass = !!app.registry.availablePlugins['ember-cli-sass'];
       let hasLess = !!app.registry.availablePlugins['ember-cli-less'];
